df3d759bd3957fcc6a3abdb2b97e9c82e2d757bd
NP User <npuser@np.io>
2017-10-30 18:58:13 +0000

    observables

---
+++ server/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const useragent = require('express-useragent');
+
+var _ = require('underscore');
+var rxjs = require('rxjs');
+
+// server setup
+const app = express();
+
+app.use(cors());
+app.options('*', cors());
+app.use(logger('dev'));
+app.use(useragent.express());
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+
+// View engine to server static page
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'server/public')));
+
+// Add routes
+var debounce = require('debounce');
+app.use('/whatever/:id', function(req, res, next) {
+  let dataSet;
+  switch(req.params.id) {
+    case "f":
+      dataSet = ["apple", "pear", "peach", "banana", "pineapple"];
+
+      // the network is bad all of a sudden
+      setTimeout(function() {
+        res.json({ data: dataSet });
+      }, 3000);
+      break;
+    case "a":
+      dataSet = ["dogs", "cats", "dolpins", "elephons", "tigers"];
+      res.json({ data: dataSet });
+      break;
+    case "s":
+      dataSet = ["basketball", "soccer", "boxing", "swimming", "running"];
+      res.json({ data: dataSet });
+      break;
+  }
+
+  /* Real world mimicing */
+  // setTimeout(function() {
+  //     console.log("debouncing..");
+  //     res.json({ data: dataSet });
+  //   }, Math.round(150 + Math.random(3) * 1200));
+
+
+});
+
+
+// Create HTTP server
+const port = process.env.PORT || '3000';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port, () => console.log(`API SERVER running on localhost:${port}`));
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
 import { Component } from '@angular/core';
+import * as Rx from 'rxjs/Rx';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+// 0f - It simply converts a list of arguments into an Observable sequence.
+// from - Creates an Observable sequence from an array or an object that can be iterated.
+// map - Transforms each element of the Observable sequence. Can be considered similar to map function of Array.
+// subscribe - This operator is basically the connecting point between an Observer and Observable.
+
 export class AppComponent {
-  title = 'app';
+
+  fruits: string[];
+  animals: string[];
+  sports: string[];
+
+  fruits$: Rx.Observable<string[]>;
+  animals$: Rx.Observable<string[]>;
+  sports$: Rx.Observable<string[]>;
+
+  observables$: Rx.Observable<string[]>;
+  whateverFromPromise: string[];
+
+  merged: Rx.Observable<string[]>;
+
+  constructor(private http: Http) {
+
+    // Observable
+    this.fruits  = ["apple", "pear", "peach", "banana", "pineapple"];
+    this.animals = ["dogs", "cats", "dolpins", "elephons", "tigers"];
+    this.sports  = ["basketball", "soccer", "boxing", "swimming", "running"];
+
+    this.fruits$ = Rx.Observable.of(this.fruits).delay(3000);;
+    this.animals$ = Rx.Observable.of(this.animals).delay(3000);
+    this.sports$ = Rx.Observable.of(this.sports).delay(5000);
+
+  }
+
+  selectObservables(what: string) {
+    this.observables$ = this.http.get(`http://localhost:3000/whatever/${what}`).map(
+      response => response.json().data
+      ).catch( error => {
+        console.log(error);
+        return Rx.Observable.throw(error);
+      });
+  }
+
+  selectPromises(what: string) {
+
+    this.myAsyncFunction(`http://localhost:3000/whatever/${what}`).then( elem => {
+      console.log(elem);
+      this.whateverFromPromise = JSON.parse(elem).data;
+      });
+
+  }
+
+  myAsyncFunction(url): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.onload = () => resolve(xhr.responseText);
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    });
+  }
+
+
 }
